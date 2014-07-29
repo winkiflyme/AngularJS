4b88df347dbaca77795cf5401e9338056c568da2
winkiflyme <Win_Ki@foxmail.com>
2014-07-29 13:19:24 +0800

    ww

---
+++ src/Test.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Administrator
+ * Date: 14-7-28
+ * Time: 下午7:45
+ * To change this template use File | Settings | File Templates.
+ */
+function  abc(){
+    alert("eeee");
+}
